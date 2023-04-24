b3f88ad86fd80a5b4029105ebe773d1209327820
thanhvux01 <101717496+thanhvux01@users.noreply.github.com>
2023-04-24 14:32:51 +0700

    fix

--- src/component/HeaderUser/index.tsx
+++ src/component/HeaderUser/index.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import styles from './styles.module.scss';
 import classNames from 'classnames/bind';
 import { Avatar } from '@mui/material';
+import { useNavigate } from 'react-router';
 let cx = classNames.bind(styles);
 
 const HeaderUser = () => {
+  const Navigate = useNavigate();
   return (
-    <div className={cx('user-wrapper')}>
+    <div className={cx('user-wrapper')} onClick={()=>{
+          Navigate('/personal')
+    }}>
      <Avatar className={cx('avatar')} >H</Avatar>
      <label className={cx('user-name')}>C.Thành</label>
      <label className={cx('role')}>Admin</label>
